4a4e8cc6ef41a80f3d7be5acbd0a90076c005b9e
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-07-11 22:59:17 +0530

    code that compares the area and perimeter of a rectangle.

---
+++ if_else/compareAreaPerimeter.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    float l,b;
+    printf("Enter Length :");
+    scanf("%f",&l);
+    printf("Enter base :");
+    scanf("%f",&b);
+
+    float area = l*b , perimeter = 2*(l+b);
+    printf("Area = %f \n Perimeter = %f",area,perimeter);
+
+    if(area>perimeter){
+        printf("\nThe area of the rectangle is bigger than its perimeter.");
+}
+    if(area<perimeter){
+        printf("The area of the rectangle is smaller than its perimeter.");
+}
+    if(area==perimeter){
+        printf("The area of the rectangle is equal to its perimeter.");
+ }
+   return 0;
+}
