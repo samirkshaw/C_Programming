827c8a34fb2f7defa1c1c42a79a2131493c0cc28
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-08-23 10:15:03 +0530

    Basic for loop demonstration printing a string n times and showing loop variable final value

---
+++ LOOPS/basicofloops.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i=i+1){
+        printf("Samir\n");
+    }
+    printf("Final value of i: %d\n",i);
+    return 0;
+}
