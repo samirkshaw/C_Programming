194288e6417f0dca89506ec6dff0b5663be7c59f
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-09-16 08:06:09 +0530

    nCr.c: compute binomial coefficient using factorial function with input validation

--- functions/nCr.c
+++ functions/nCr.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int factorial(int f){
-    int x=1;
-    for(int i =1;i<=f;i++){
-        x=x*i;
+int factorial(int f) {
+    int x = 1;
+    for (int i = 1; i <= f; i++) {
+        x = x * i;
     }
     return x;
 }
-int main(){
-    int n,r;
-    printf("Enter the value of n : ");
-    scanf("%d",&n);
-    printf("Enter the value of r : ");
-    scanf("%d",&r);
-    if(r>n){
-        printf("value of 'r' cant be greater than 'n'. Please enter valid inputs.");
-    }
-    else{
-        int nCr = factorial(n)/(factorial(r)*factorial(n-r));
 
+int main() {
+    int n, r;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("Enter the value of r: ");
+    scanf("%d", &r);
+    if (r > n) {
+        printf("Value of 'r' cannot be greater than 'n'. Please enter valid inputs.\n");
+    } else {
+        int nCr = factorial(n) / (factorial(r) * factorial(n - r));
         printf("nCr = %d\n", nCr);
     }
     return 0;
