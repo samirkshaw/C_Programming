9ff86b8bf3247c40c893dbded1c609c5359c3d8c
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-09-18 16:15:25 +0530

    Print array element addresses using %p format specifier in adress.c

---
+++ Arrays/adress.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+     int arr[5]={2,3,4,5,6};
+     printf("\n%p",&arr[0]);
+     printf("\n%p",&arr[1]);
+     printf("\n%p",&arr[2]);
+     printf("\n%p",&arr[3]);
+     printf("\n%p",&arr[4]);
+     return 0;
+}
