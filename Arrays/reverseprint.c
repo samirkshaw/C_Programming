4c60ba0b560adbe40ef7083348dbad05526b090e
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-09-22 08:42:00 +0530

    Implement reverse array printing using loop from end to start in reverseprint.c

---
+++ Arrays/reverseprint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array in reverse order: ");
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
